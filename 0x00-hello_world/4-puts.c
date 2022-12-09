072b76b7c371aca4fb4701a62c95a3cdb73c71cc
kleland <kwamenacleland@gmail.com>
2022-12-08 19:02:23 -0800

    3rd commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Main function > entry point
+*
+*
+*Return: return 0 always
+*/
+int main(void)
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
